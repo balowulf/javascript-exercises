9a061d205a79e4ec3bee771d626c1519471583a5
Chris Balow Jr <cmbalow@illuminateed.net>
2018-12-03 16:37:41 -0500

    completed sumAll exercise

--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -1,5 +1,30 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    // (1, 4)
+    // 1, 2, 3, 4
+    // 1 + 2 + 3 + 4 = 10
+    // 4-1 = 3
 
+    // 1 + 4 = 5
+    // 4 - 1 = 3
+    if (a < 0 || b < 0 || (typeof a != 'number' || typeof b != 'number')) {
+        return 'ERROR';
+    } else {
+        let sumArr = [a, b];
+        if (b > a) {
+            min = a;
+            max = b;
+        } else if (a > b) {
+            max = a;
+            min = b;
+        }
+        let sumDiff = max - min;
+        while (sumDiff > min) {
+            sumArr.push(sumDiff);
+            sumDiff--;
+        }
+        return sumArr.reduce((a, b) => a + b, 0);
+    }
+    
 }
 
 module.exports = sumAll
