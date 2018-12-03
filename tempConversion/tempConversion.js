8a8d0ec0792f5065d85493e97a463efb603c63dc
Chris Balow Jr <cmbalow@illuminateed.net>
2018-12-03 16:48:39 -0500

    completed tempConversion exercise

--- tempConversion/tempConversion.js
+++ tempConversion/tempConversion.js
@@ -1,9 +1,13 @@
-const ftoc = function() {
-
+const ftoc = function(temp) {
+    rawC = (temp - 32) * 5/9;
+    roundedC = Math.round(rawC * 10) / 10;
+    return roundedC;
 }
 
-const ctof = function() {
-
+const ctof = function(temp) {
+    rawF = (temp * 9/5) + 32;
+    roundedF = Math.round(rawF * 10) / 10;
+    return roundedF;
 }
 
 module.exports = {
