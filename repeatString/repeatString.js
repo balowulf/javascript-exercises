4cbb92ccf3c38061ac5ae06fa4a09ef23d1ed832
Chris Balow Jr <cmbalow@illuminateed.net>
2018-12-02 20:24:42 -0500

    completed repeatString exercise

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(str, times) {
+    let newStr = '';
+    if (times < 0) {
+        return 'ERROR';
+    } else {
+        for (i = times; i > 0; i--) {
+            newStr += str;
+        }
+        return newStr; 
+    }
 }
 
 module.exports = repeatString
