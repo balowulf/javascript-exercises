443c415a58f47a68e8e23a8db20cbd9aa66f82dd
Chris Balow Jr <cmbalow@illuminateed.net>
2018-12-03 10:35:30 -0500

    removeFromArray initial commit -- in progress, not working for multiple args

--- removeFromArray/removeFromArray.js
+++ removeFromArray/removeFromArray.js
@@ -1,5 +1,29 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, vals) {
+    let args = Array.prototype.slice.call(arguments, 1);
+    let i = args.length;
+    while (i > 0) {
+        array = array.filter(function(item) {
+            return item != vals;
+        });
+        args.splice(0, 1);
+        i--;
+    }
+    return array;
 }
 
 module.exports = removeFromArray
+
+
+/**
+ *  Removing multile items
+ * 
+ *  let forDeletion = [2, 3, 5]
+
+    let arr = [1, 2, 3, 4, 5, 3]
+
+    arr = arr.filter(item => !forDeletion.includes(item))
+    
+
+    console.log(arr)
+    // [ 1, 4 ]
+ */
