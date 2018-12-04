294f549f2ca318dec4f85cbdac2718f60555745a
Chris Balow Jr <cmbalow@illuminateed.net>
2018-12-03 20:14:55 -0500

    completed leapYears exercise

--- leapYears/leapYears.js
+++ leapYears/leapYears.js
@@ -1,5 +1,10 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    return ((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0);
 }
 
 module.exports = leapYears
+
+// here is a leap year every year whose number is perfectly divisible by four - except for years which are both divisible by 100 and not divisible by 400
+// true: year % 4 === 0
+// false: year / 100 % 1 == 0
+// flase
