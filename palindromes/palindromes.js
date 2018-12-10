c305531fd9915d1041d461bd5b7bedcab5837801
Chris Balow Jr <cmbalow@illuminateed.net>
2018-12-10 09:52:10 -0500

    completed palindromes exercise

--- palindromes/palindromes.js
+++ palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function() {
-
+const palindromes = function(string) {
+    let regex = /[^\w]/g;
+    newString = string.replace(regex, '').toLowerCase();
+    reverseString = newString.split('').reverse().join('');
+    if (reverseString === newString) {
+        return true;
+    } else {
+        return false;
+    }
 }
 
 module.exports = palindromes
