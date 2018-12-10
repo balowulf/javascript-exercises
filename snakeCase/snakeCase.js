7fa263ddece1b27bcf24b880559c1a2b2b51d1ad
Chris Balow Jr <cmbalow@illuminateed.net>
2018-12-10 10:20:13 -0500

    getting started with snakeCase

--- snakeCase/snakeCase.js
+++ snakeCase/snakeCase.js
@@ -1,4 +1,15 @@
-const snakeCase = function() {
+const snakeCase = function(str) {
+    // preprocess the string
+    // // trim everything outside the first and last words
+    // // set everything toLowerCase
+    // replace the following with underscores:
+    // // anything that is between two words
+    // // camelCases
+    let trim = /\W+$/g;
+    let between = /[^\w+.\w+]/g;
+    str = str.replace(trim, '').toLowerCase();
+    str = str.replace(between, '_');
+    console.log(str);
 
 }
 
