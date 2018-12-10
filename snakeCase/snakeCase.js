5a008b549b9a287b73b5b3b69b7c9983c48d41ae
Chris Balow Jr <cmbalow@illuminateed.net>
2018-12-10 16:41:25 -0500

    refactored for slightly more elegant regex approach

--- snakeCase/snakeCase.js
+++ snakeCase/snakeCase.js
@@ -1,8 +1,7 @@
 const snakeCase = function(str) {
-    let trim = /[^A-Z]/gmi;
     let trailing = /_{2,}$/gmi;
     let multiples = /_{2,}/gmi;
-    str = str.replace(trim, '_').toLowerCase();
+    str = str.split(/[^\b\w+\b]/gmi).join('_').toLowerCase();
     str = str.replace(trailing, '');
     str = str.replace(multiples, '_');
     return str;
