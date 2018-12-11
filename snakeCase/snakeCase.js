89fc4a70aa8e9799c5d25e941f593fe5912b12e2
Chris Balow Jr <cmbalow@illuminateed.net>
2018-12-10 19:25:56 -0500

    snakeCase solution

--- snakeCase/snakeCase.js
+++ snakeCase/snakeCase.js
@@ -1,10 +1,19 @@
-const snakeCase = function(str) {
-    let trailing = /_{2,}$/gmi;
-    let multiples = /_{2,}/gmi;
-    str = str.split(/[^\b\w+\b]/gmi).join('_').toLowerCase();
-    str = str.replace(trailing, '');
-    str = str.replace(multiples, '_');
-    return str;
-}
+const snakeCase = function(string) {
+    // wtf case
+    string = string.replace(/\.\./g, " ");
+  
+    // this splits up camelcase IF there are no spaces in the word
+    if (string.indexOf(" ") < 0) {
+      string = string.replace(/([A-Z])/g, " $1");
+    }
+  
+    return string
+      .trim()
+      .toLowerCase()
+      .replace(/[,\?\.]/g, "")
+      .replace(/\-/g, " ")
+      .split(" ")
+      .join("_");
+  };
 
 module.exports = snakeCase
