4cb012cf640d6ce41a8b566217c488349b5e77b0
Chris Balow Jr <cmbalow@illuminateed.net>
2018-12-11 17:03:11 -0500

    completed fibonacci

--- fibonacci/fibonacci.js
+++ fibonacci/fibonacci.js
@@ -1,5 +1,11 @@
-const fibonacci = function() {
-
+const fibonacci = (n) => {
+    if (n < 0) {
+        return 'OOPS';
+    }
+    if (n == 0 || n == 1) {
+        return n;
+    }
+    return fibonacci(n - 1) + fibonacci(n - 2);
 }
 
 module.exports = fibonacci
